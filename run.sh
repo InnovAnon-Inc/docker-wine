82563801375d7d7056e9c4bee0ea6c4fc9eba820
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-05-24 16:33:48 -0500

    auto update

--- run.sh
+++ run.sh
@@ -1,13 +1,10 @@
 #! /bin/bash
 set -exu
 [[ $# -eq 0 ]]
+cd "`dirname "$(readlink -f "$0")"`"
 
-sudo             -- \
-nice -n +20      -- \
-sudo -u `whoami` -- \
-docker-compose build
-
-docker push innovanon/docker-wine:latest || :
+command -v docker ||
+curl https://raw.githubusercontent.com/InnovAnon-Inc/repo/master/get-docker.sh | bash
 
 trap 'docker-compose down' 0
 
@@ -15,5 +12,11 @@ xhost +local:`whoami`
 sudo             -- \
 nice -n +20      -- \
 sudo -u `whoami` -- \
-docker-compose up --force-recreate
+docker-compose up --build --force-recreate
+
+docker-compose push
+( #git pull
+git add .
+git commit -m "auto commit by $0"
+git push ) || :
 
